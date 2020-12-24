711ed03d3e92c02b308f37988442952a4a05a4e0
maekawatoshiki <konndennsa@gmail.com>
2020-12-25 01:08:55 +0900

    Support CI

---
+++ run_kcov.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+RUST_BACKTRACE=full cargo test -- --test-threads=1
+REPORT=$(find ./target/debug -maxdepth 2 -regex '.+/deps/.*' -a ! -regex '.+\.\(d\|rlib\|rmeta\|so\)')
+for file in $REPORT; do  
+  echo $file
+  kcov --include-pattern=vicis/src --exclude-pattern=/.cargo ./target/cov "$file"
+done
+bash <(curl -s https://codecov.io/bash) -s ./target/cov -t $1
