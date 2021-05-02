a7825ae68863f3a1b5289fba1df5fa49c748cf4d
maekawatoshiki <konndennsa@gmail.com>
2021-05-02 22:02:37 +0900

    Delete unnecessary files

--- run_kcov.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-RUST_BACKTRACE=full cargo test -- --test-threads=1
-REPORT=$(find ./target/debug -maxdepth 2 -regex '.+/deps/.*' -a ! -regex '.+\.\(d\|rlib\|rmeta\|so\)')
-for file in $REPORT; do  
-  echo $file
-  kcov --include-pattern=vicis/src --exclude-pattern=/.cargo ./target/cov "$file"
-done
-bash <(curl -s https://codecov.io/bash) -s ./target/cov -t $1
