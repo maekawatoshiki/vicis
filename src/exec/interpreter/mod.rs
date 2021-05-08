273498dbb6710e7b4c3311c8b1b2099db0287388
maekawatoshiki <konndennsa@gmail.com>
2021-05-09 01:13:44 +0900

    Refine interpreter: split functions

--- src/exec/interpreter/mod.rs
+++ src/exec/interpreter/mod.rs
@@ -8,15 +8,13 @@ use crate::ir::{
     },
     module::Module,
     types::{Type, TypeId, Types},
-    value::{ConstantData, ConstantInt, Value},
+    value::{ConstantData, ConstantInt, Value, ValueId},
 };
 use std::alloc;
 
 pub fn run_function(module: &Module, func_id: FunctionId) -> Option<GenericValue> {
     let func = &module.functions()[func_id];
-
     let mut stack = frame::StackFrame::new(module, func);
-
     let mut block = func.layout.first_block?;
 
     'main: loop {
@@ -32,71 +30,24 @@ pub fn run_function(module: &Module, func_id: FunctionId) -> Option<GenericValue
                     num_elements,
                     align,
                 } => run_alloca(&mut stack, inst_id, tys, num_elements, *align),
-                Operand::Store {
-                    tys,
-                    args,
-                    align: _,
-                } => {
-                    let ty = tys[0];
-                    let src = args[0];
-                    let dst = args[1];
-                    let dst_addr = stack.get_val(dst).unwrap();
-                    match func.data.value_ref(src) {
-                        Value::Constant(ConstantData::Int(ConstantInt::Int32(i))) => unsafe {
-                            *(dst_addr.to_ptr().unwrap() as *mut i32) = *i;
-                        },
-                        Value::Instruction(id) if matches!(&*func.types.get(ty), Type::Int(32)) => unsafe {
-                            *(dst_addr.to_ptr().unwrap() as *mut i32) =
-                                stack.get_inst_val(*id).unwrap().to_i32().unwrap();
-                        },
-                        e => todo!("{:?}", e),
-                    }
-                }
-                Operand::Load {
-                    tys,
-                    addr,
-                    align: _,
-                } => {
-                    let ty = tys[0];
-                    let addr = stack.get_val(*addr).unwrap();
-                    match &*func.types.get(ty) {
-                        Type::Int(32) => stack.add_inst_val(
-                            inst_id,
-                            GenericValue::Int32(unsafe { *(addr.to_ptr().unwrap() as *const i32) }),
-                        ),
-                        _ => todo!(),
-                    };
+                Operand::Store { tys, args, align } => run_store(&mut stack, tys, args, *align),
+                Operand::Load { tys, addr, align } => {
+                    run_load(&mut stack, inst_id, tys, *addr, *align)
                 }
                 Operand::IntBinary {
                     ty: _,
                     nsw: _,
                     nuw: _,
                     args,
-                } => {
-                    let x = stack.get_val(args[0]).unwrap();
-                    let y = stack.get_val(args[1]).unwrap();
-                    match inst.opcode {
-                        Opcode::Add => stack.add_inst_val(inst_id, add(x, y).unwrap()),
-                        Opcode::Sub => stack.add_inst_val(inst_id, sub(x, y).unwrap()),
-                        _ => todo!(),
-                    };
-                }
-                Operand::ICmp { ty: _, args, cond } => {
-                    let x = stack.get_val(args[0]).unwrap();
-                    let y = stack.get_val(args[1]).unwrap();
-                    let res = match cond {
-                        ICmpCond::Slt => slt(x, y).unwrap(),
-                        _ => todo!(),
-                    };
-                    stack.add_inst_val(inst_id, res);
-                }
+                } => run_int_binary(&mut stack, inst_id, inst.opcode, args),
+                Operand::ICmp { ty: _, args, cond } => run_icmp(&mut stack, inst_id, args, *cond),
                 Operand::CondBr { arg, blocks } => {
                     let arg = stack.get_val(*arg).unwrap();
-                    if matches!(arg, GenericValue::Int1(true)) {
-                        block = blocks[0];
+                    block = blocks[if matches!(arg, GenericValue::Int1(true)) {
+                        0
                     } else {
-                        block = blocks[1];
-                    }
+                        1
+                    }];
                     continue 'main;
                 }
                 Operand::Br { block: b } => {
@@ -142,6 +93,66 @@ fn run_alloca(
         alloc::alloc(alloc::Layout::from_size_align(alloc_sz, alloc_align).expect("layout err"))
     };
     stack.add_inst_val(id, GenericValue::Ptr(ptr));
+}
+
+fn run_store(stack: &mut frame::StackFrame, tys: &[TypeId], args: &[ValueId], _align: u32) {
+    let ty = tys[0];
+    let src = args[0];
+    let dst = args[1];
+    let dst_addr = stack.get_val(dst).unwrap();
+    match stack.func.data.value_ref(src) {
+        Value::Constant(ConstantData::Int(ConstantInt::Int32(i))) => unsafe {
+            *(dst_addr.to_ptr().unwrap() as *mut i32) = *i;
+        },
+        Value::Instruction(id) if matches!(&*stack.func.types.get(ty), Type::Int(32)) => unsafe {
+            *(dst_addr.to_ptr().unwrap() as *mut i32) =
+                stack.get_inst_val(*id).unwrap().to_i32().unwrap();
+        },
+        e => todo!("{:?}", e),
+    }
+}
+
+fn run_load(
+    stack: &mut frame::StackFrame,
+    id: InstructionId,
+    tys: &[TypeId],
+    addr: ValueId,
+    _align: u32,
+) {
+    let ty = tys[0];
+    let addr = stack.get_val(addr).unwrap();
+    match &*stack.func.types.get(ty) {
+        Type::Int(32) => stack.add_inst_val(
+            id,
+            GenericValue::Int32(unsafe { *(addr.to_ptr().unwrap() as *const i32) }),
+        ),
+        _ => todo!(),
+    };
+}
+
+fn run_int_binary(
+    stack: &mut frame::StackFrame,
+    id: InstructionId,
+    opcode: Opcode,
+    args: &[ValueId],
+) {
+    let x = stack.get_val(args[0]).unwrap();
+    let y = stack.get_val(args[1]).unwrap();
+    match opcode {
+        Opcode::Add => stack.add_inst_val(id, add(x, y).unwrap()),
+        Opcode::Sub => stack.add_inst_val(id, sub(x, y).unwrap()),
+        _ => todo!(),
+    };
+}
+
+fn run_icmp(stack: &mut frame::StackFrame, id: InstructionId, args: &[ValueId], cond: ICmpCond) {
+    let x = stack.get_val(args[0]).unwrap();
+    let y = stack.get_val(args[1]).unwrap();
+    let res = match cond {
+        ICmpCond::Slt => slt(x, y).unwrap(),
+        _ => todo!(),
+    };
+    stack.add_inst_val(id, res);
 }
 
 // Utils
