eb980441a489d35c6e5ad6dde6024365f793ff03
maekawatoshiki <konndennsa@gmail.com>
2022-02-13 19:04:07 +0900

    [core] Fix typos (e.g. not 'linkonceodr' but 'linkonce_odr')

--- core/src/parser/assembly/linkage.rs
+++ core/src/parser/assembly/linkage.rs
@@ -8,9 +8,11 @@ pub fn parse(source: &str) -> IResult<&str, Linkage, VerboseError<&str>> {
         map(tag("external"), |_| Linkage::External),
         map(tag("externalweak"), |_| Linkage::ExternalWeak),
         map(tag("availableexternally"), |_| Linkage::AvailableExternally),
-        map(tag("linkonceany"), |_| Linkage::LinkOnceAny),
-        map(tag("linkonceodr"), |_| Linkage::LinkOnceODR),
-        map(tag("linkonceodrautohide"), |_| Linkage::LinkOnceODRAutoHide),
+        map(tag("linkonce_any"), |_| Linkage::LinkOnceAny),
+        map(tag("linkonce_odr"), |_| Linkage::LinkOnceODR),
+        map(tag("linkonce_odrautohide"), |_| {
+            Linkage::LinkOnceODRAutoHide
+        }),
         map(tag("weakany"), |_| Linkage::WeakAny),
         map(tag("weakodr"), |_| Linkage::WeakODR),
         map(tag("common"), |_| Linkage::Common),
