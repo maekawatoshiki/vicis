93f97762fc09ff6f0cc80c51f8962efb727db665
maekawatoshiki <konndennsa@gmail.com>
2022-02-23 01:26:27 +0900

    [core] Implement Value::display()

--- core/src/ir/value/mod.rs
+++ core/src/ir/value/mod.rs
@@ -1,6 +1,6 @@
 use super::{
     function::{data::Data, instruction::InstructionId},
-    types::Types,
+    types::{Typed, Types},
 };
 use id_arena::Id;
 
@@ -42,26 +42,8 @@ impl Value {
         }
     }
 
-    pub fn to_string(&self, _data: &Data, types: &Types) -> String {
-        match self {
-            Self::Constant(c) => c.to_string(types),
-            Self::Instruction(id) => {
-                format!("%I{}", id.index())
-            }
-            Self::Argument(n) => format!("%A{}", n),
-            Self::InlineAsm(InlineAsm {
-                body,
-                constraints,
-                sideeffect,
-            }) => {
-                format!(
-                    "asm {}\"{}\", \"{}\"",
-                    if *sideeffect { "sideeffect " } else { "" },
-                    constraints,
-                    body
-                )
-            }
-        }
+    pub fn display<'a>(&'a self, data: &'a Data, types: &'a Types) -> DisplayValue<'a> {
+        DisplayValue(self, data, types)
     }
 }
 
@@ -74,5 +56,36 @@ impl From<i32> for Value {
 impl From<i64> for Value {
     fn from(i: i64) -> Self {
         Self::Constant(ConstantInt::Int64(i).into())
+    }
+}
+
+pub struct DisplayValue<'a>(pub &'a Value, pub &'a Data, pub &'a Types);
+
+impl std::fmt::Display for DisplayValue<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.0 {
+            Value::Constant(c) => write!(f, "{} {}", self.2.to_string(c.ty()), c.to_string(self.2)),
+            Value::Instruction(id) => {
+                let inst = self.1.inst_ref(*id);
+                if let Some(dest) = &inst.dest {
+                    write!(f, "%{}", dest)
+                } else {
+                    write!(f, "%I{}", id.index()) // TODO
+                }
+            }
+            Value::Argument(n) => write!(f, "%A{}", n),
+            Value::InlineAsm(InlineAsm {
+                body,
+                constraints,
+                sideeffect,
+            }) => write!(
+                f,
+                "{}asm {}\"{}\", \"{}\"",
+                if *sideeffect { "sideeffect " } else { "" },
+                constraints,
+                body,
+                body
+            ),
+        }
     }
 }
