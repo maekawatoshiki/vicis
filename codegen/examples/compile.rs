25192753dcacc5d057dced3d7936a7e94a33a5aa
maekawatoshiki <konndennsa@gmail.com>
2022-03-20 00:34:55 +0900

    [codegen] Add 'examples/compile.rs'

---
+++ codegen/examples/compile.rs
@@ -0,0 +1,33 @@
+extern crate structopt;
+extern crate vicis_codegen;
+extern crate vicis_core;
+
+use std::fs::{self, File};
+use std::io::Write;
+use structopt::StructOpt;
+use vicis_codegen::codegen::{isa::x86_64::X86_64, lower::compile_module};
+use vicis_core::ir::module::Module;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "i")]
+pub struct Opt {
+    pub ir_file: String,
+
+    #[structopt(short = "o")]
+    pub out_file: Option<String>,
+}
+
+fn main() {
+    env_logger::init();
+    let opt = Opt::from_args();
+    let ir = fs::read_to_string(&opt.ir_file).expect("failed to load *.ll file");
+    let module = Module::try_from(ir.as_str()).expect("failed to parse LLVM Assembly");
+    let module = compile_module(X86_64, &module).unwrap();
+    File::create(
+        opt.out_file
+            .unwrap_or_else(|| opt.ir_file.trim_end_matches(".ll").to_owned() + ".s"),
+    )
+    .expect("failed to create output file")
+    .write(format!("{}", module).as_bytes())
+    .unwrap();
+}
