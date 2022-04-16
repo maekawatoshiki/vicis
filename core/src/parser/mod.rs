31c1ba7d1203c795b15eacccdb515c36e3a493af
maekawatoshiki <konndennsa@gmail.com>
2022-04-16 19:59:28 +0900

    Use lalrpop

--- core/src/parser/mod.rs
+++ core/src/parser/mod.rs
@@ -1,2 +1,12 @@
 pub mod assembly;
 pub mod bitcode;
+mod parser;
+
+#[test]
+fn parsertest() {
+    let mut module = crate::ir::module::Module::default();
+    parser::ModuleParser::new()
+        .parse(&mut module, r#"source_filename = "a.c""#)
+        .unwrap();
+    println!("{:?}", module);
+}
