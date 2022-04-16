af546dcef88849dd8fdab2f4a36044ae7fe3fe93
maekawatoshiki <konndennsa@gmail.com>
2022-04-16 20:35:33 +0900

    Support attributes

--- core/src/parser/mod.rs
+++ core/src/parser/mod.rs
@@ -6,7 +6,14 @@ mod parser;
 fn parsertest() {
     let mut module = crate::ir::module::Module::default();
     parser::ModuleParser::new()
-        .parse(&mut module, r#"source_filename = "a.c""#)
+        .parse(
+            &mut module,
+            r#"
+            source_filename = "a.c"
+            target datalayout = "xxxxx"
+            attributes #0 = { alwaysinline  }
+            "#,
+        )
         .unwrap();
     println!("{:?}", module);
 }
